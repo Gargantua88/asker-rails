0a4385ba7e9f6953c4fa75e2cdacd36681e9290c
gargantua <istroganovi@gmail.com>
2018-12-28 20:21:38 +0300

    fix bugs with database

--- db/migrate/20181228150350_create_hashtags.rb
+++ db/migrate/20181228150350_create_hashtags.rb
@@ -6,9 +6,4 @@ class CreateHashtags < ActiveRecord::Migration[5.2]
       t.timestamps
     end
   end
-
-  create_table :hashtags_questions do |t|
-    t.references :question, foreign_key: true
-    t.references :hashtag, foreign_key: true
-  end
 end
